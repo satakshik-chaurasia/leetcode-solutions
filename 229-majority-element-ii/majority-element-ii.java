abbe8dd504b5528cbc606d0fd5e857f4f96d3696
satakshik-chaurasia <satakshik338@gmail.com>
2026-01-22 00:22:58 +0530

    Time: 2 ms (99.63%) | Memory: 52.5 MB (56.29%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,17 +1,44 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
+        int count1=0;
+        int c1=0;
+        int c2=0;
+        int count2=0;
         List<Integer> result=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++){
-           int num=nums[i];
-            map.put(num,map.getOrDefault(num,0)+1);
+       for(int i=0;i<n;i++){
+        int num=nums[i];
+       
+       if(count1==0&&num!=c2){
+        c1=num;
+        count1=1;
+       }
+       else if(count2==0&&num!=c1){
+        c2=num;
+        count2=1;
+       }
+       else if(num==c1){
+        count1++;
+       }
+       else if(num==c2){
+        count2++;
+       }
+       else{
+        count1--;
+        count2--;
+       }}
+       count1 = 0;
+        count2 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == c1) count1++;
+            else if(nums[i] == c2) count2++;
         }
-        for(int x:map.keySet()){
-            if(map.get(x)>n/3){
-                result.add(x);
-            }
-        }
+
+        // Phase-3 (check > n/3)
+        if(count1 > n/3) result.add(c1);
+        if(count2 > n/3) result.add(c2);
+
         return result;
-    }
-}
+       
+        
+    }}
