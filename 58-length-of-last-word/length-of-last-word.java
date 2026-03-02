b99391ca6efade3d386b834cad640bf5e2993c3b
satakshik-chaurasia <satakshik338@gmail.com>
2026-03-02 15:59:20 +0530

    Time: 1 ms (32.42%) | Memory: 43 MB (70.56%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,14 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int i=s.length()-1;
-        int count=0;
-        while(i>=0&&s.charAt(i)==' '){
-            i--;
-        }
-        while(i>=0&&s.charAt(i)!=' '){
-            count++;
-            i--;
-        }
-        return count;
+        s=s.trim();
+        String []word=s.split(" ");
+        return word[word.length-1].length();
     }
 }
