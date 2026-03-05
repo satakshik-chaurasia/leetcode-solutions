5408c47f62745473068506762b6fc8e25a026919
satakshik-chaurasia <satakshik338@gmail.com>
2026-03-05 11:18:37 +0530

    Time: 6 ms (42.68%) | Memory: 44.4 MB (74.58%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] arr1=s.toCharArray();
-        char[] arr2=t.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        if(Arrays.equals(arr1,arr2)){
-            return true;
-        }
-        else{
+        if(s.length()!=t.length()){
             return false;
         }
+        int [] freq=new int[26];
+      for(int i=0;i<s.length();i++){
+        freq[s.charAt(i)-'a']++;
+        freq[t.charAt(i)-'a']--;
+      }
+      for(int x: freq){
+        if(x!=0){
+            return false;
+        }
+        
+      }
+       return true; 
     }
 }
