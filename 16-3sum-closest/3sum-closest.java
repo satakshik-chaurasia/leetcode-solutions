67ab43bd99635098c3451e4516cf13afec02fde7
satakshik-chaurasia <satakshik338@gmail.com>
2026-04-13 23:03:32 +0530

    Time: 18 ms (41.41%) | Memory: 45.5 MB (71.77%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,41 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-
         Arrays.sort(nums);
-        int n = nums.length;
-
-        int ans = nums[0] + nums[1] + nums[2];
-
-        for(int i = 0; i < n - 2; i++) {
-
-            // skip same first element (optional optimization)
-            if(i > 0 && nums[i] == nums[i-1])
+        int ans = nums[0]+nums[1]+nums[2];
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n-2;i++){
+            if(i>0&&nums[i]==nums[i-1]){
                 continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while(left < right) {
-
-                int curr = nums[i] + nums[left] + nums[right];
-
-                // update answer
-                if(Math.abs(target - curr) < Math.abs(target - ans))
-                    ans = curr;
-
-                // pointer movement
-                if(curr > target) {
-                    right--;
-                }
-                else if(curr < target) {
-                    left++;
-                }
-                else {
-                    return target;   // exact match
-                }
             }
+            int l=i+1;
+            int r=n-1;
+        while(l<r){
+            sum=nums[i]+nums[l]+nums[r];
+           if(Math.abs(target-sum)<Math.abs(target-ans)){
+            ans=sum;
+           }
+           if(sum<target){
+            l++;
+           }
+           else if(sum>target){
+            r--;
+           }
+           else{
+            return target;
+           }
+        }
         }
         return ans;
     }
